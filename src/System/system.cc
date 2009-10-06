36d520c6c56dc3a7e3d0ff445d7d2fdda7ab99c6
A. Bram Neijt <bneijt@gmail.com>
2009-10-06 23:29:56 +0200

    VectorRing to boost::circular_buffer

--- src/System/system.cc
+++ src/System/system.cc
@@ -23,7 +23,7 @@
 
 int System::system(std::string const &command, bool simulate) throw (Problem)
 {
-	VectorRing<string> output(1024); //TODO use boost circular buffer
+	boost::circular_buffer_space_optimized<string> output(1024); //TODO use boost circular buffer
   if(Options::showCommands)
   	cout << command << "\n";
   
