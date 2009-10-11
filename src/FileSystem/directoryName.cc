fd3a0097f7bd43f0208a1519d1dbb58c9b342c6c
A. Bram Neijt <bneijt@gmail.com>
2009-10-11 19:39:47 +0200

    Source header replacement, author stripped

--- src/FileSystem/directoryName.cc
+++ src/FileSystem/directoryName.cc
@@ -1,21 +1,22 @@
 /*
-  ccbuild - A strict developer's build utility
-  Copyright (C) 2008  A. Bram Neijt <bneijt@gmail.com>
+  This file is part of ccbuild.
 
-  This program is free software: you can redistribute it and/or modify
+  ccbuild is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  ccbuild is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+  along with ccbuild.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
+
 
 
 
