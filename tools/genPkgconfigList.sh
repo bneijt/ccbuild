826fc7f01e347cd8139372a07d5d32f2306e3372
A. Bram Neijt <bneijt@gmail.com>
2009-10-26 23:25:24 +0100

    Build system update
    - Update of build system, based on 2.0.0alpha
    - Changed version to 2.0.0
    - Changed SF to new homepage url

--- tools/genPkgconfigList.sh
+++ tools/genPkgconfigList.sh
@@ -17,7 +17,7 @@
 # Small script to generate a pkg-config list for a given package
 #
 # Use this script like: genPkgconfigList.sh gtkmm-2.4
-# PLEASE NOTE: This creates large list. Only use when you are really lazy :-)
+# PLEASE NOTE: This creates large list. Only use when you are really lazy ;-)
 HELP="Usage: $0 <pkg-config package name>"
 if [ -z "$1" ];then
 	echo "$HELP"
@@ -30,7 +30,6 @@ PATHS=`pkg-config --cflags "$PACKAGE"|grep -o -e '-I[^ ]*'|sed -e 's/^-I//'`
 echo '# Autolis generated for ccbuild resolutions of' $PACKAGE
 echo '# this file was automatically generated using:'
 echo "#  $0 $@"
-echo '# For more information visit: http://ccbuild.sourceforge.net'
 echo '#'
 
 for path in $PATHS; do
