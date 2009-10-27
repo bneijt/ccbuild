b74ea70811f57078e03d8f01216422e533dbd3f0
A. Bram Neijt <bneijt@gmail.com>
2009-10-27 21:36:49 +0100

    Behavior change: strip ^lib of program name
    - Strip any lib prefix of the program name

--- src/system/projectName.cc
+++ src/system/projectName.cc
@@ -26,6 +26,9 @@ std::string System::projectName()
 	if(d == "src")
 		d = FileSystem::fileName(FileSystem::cleanPath(FileSystem::cwd() + "/.."));
 
+  //Strip ^lib
+  if(String(d).startsWith("lib"))
+    d = d.substr(3);
 	//TODO Strip any version number
 	// Any single occuring _ or - might be the seperator
 	// The last of these is the seperation point
