5f87e3be5c1246bec41b87dd179ff567bf408c18
A. Bram Neijt <bneijt@gmail.com>
2009-11-28 17:26:17 +0100

    OpenSSL to gnutls-openssl conversion
    - Because of licensing problems with openssl, switch to gnutls-openssl
      for the md5 algorithm. Closes #14

--- src/MD5Info/contentHash.cc
+++ src/MD5Info/contentHash.cc
@@ -22,30 +22,29 @@ std::string const &MD5Info::contentHash(std::string const &filename)
   OpenMP::ScopedLock lock(d_contentLock);
 	if(d_content.count(filename) == 0)
 	{
-	  //Create the hex hash of the file
-	  unsigned char md[EVP_md5()->md_size];
-	  EVP_MD_CTX context;
-	  
-	  //Done by the INIT below. EVP_MD_CTX_init(&context, EVP_md5(void));
-	  EVP_DigestInit(&context, EVP_md5());
+
+    //#define MD5_DIGEST_LENGTH 16
+
+	  unsigned char md[MD5_DIGEST_LENGTH];
+    MD5_CTX context;
+    MD5_Init(&context);
 
     //Start updating with the file content
-    size_t const bufferSize = EVP_md5()->block_size;
+    size_t const bufferSize = 1024*512/8; //512-bit block multiple for update
     char *buffer = new char[bufferSize];
     ifstream ifile(filename.c_str());
     while(ifile)
     {
       ifile.read(buffer, bufferSize);
-  	  EVP_DigestUpdate(&context, buffer, ifile.gcount());
+  	  MD5_Update(&context, buffer, ifile.gcount());
     }
     delete[] buffer;
     
-    unsigned int size;
-	  EVP_DigestFinal(&context, &md[0], &size); //context is automatically cleaned-up
-	  char hex_hash[(EVP_md5()->md_size * 2) + 1];
-	  for(size_t i = 0; i < size; ++i)
+	  MD5_Final(&md[0], &context); //context is automatically cleaned-up
+	  char hex_hash[(MD5_DIGEST_LENGTH * 2) + 1];
+	  for(size_t i = 0; i < MD5_DIGEST_LENGTH; ++i)
 	    sprintf(&hex_hash[i*2], "%02x", md[i]);
-    hex_hash[(EVP_md5()->md_size * 2)] = '\0';
+    hex_hash[(MD5_DIGEST_LENGTH * 2)] = '\0';
     d_content[filename] = string(&hex_hash[0]);
     _debugLevel2("Calculated content hash " << filename << " == " << d_content[filename]);
 	}
