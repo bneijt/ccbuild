93af7b9bf3dacfd65803e06bb543d8ab4afa3bc4
A. Bram Neijt <bneijt@gmail.com>
2014-08-07 21:16:58 +0200

    Add yylex and username files
    yylex is generated, but should allow for travis-ci builds

---
+++ src/system/username.cc
@@ -0,0 +1,34 @@
+/*
+  This file is part of ccbuild.
+
+  ccbuild is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ccbuild is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ccbuild.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "system.ih"
+
+std::string System::username() throw (Problem) {
+    size_t uid = getuid();
+    struct passwd *pwd;
+    std::string username;
+
+    while ((pwd = getpwent()))
+    {
+        if (pwd->pw_uid == uid)
+        {
+            username = std::string(pwd->pw_name);
+            endpwent();
+            return username;
+        }
+    }
+    throw Problem(Problem::Unable, "Unable to determine username for current process");
+}
